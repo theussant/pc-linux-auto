812c7a3aad87943ffe2dce9474b17567332e4fdb
theus6 <msantossilva005@gmail.com>
2022-10-22 23:18:57 -0300

    First commit

---
+++ auto.bash
@@ -0,0 +1,37 @@
+# !/bin/bash
+
+# chmod +x setup-pc.bash
+# ./setup-pc.bash
+
+# Made for Ubuntu 20.04
+
+# Install terminator
+sudo apt install terminator
+
+# Install git
+sudo apt-get install git
+
+# Install vscode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+# Install Brave
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+# Install tweaks
+sudo apt install gnome-tweaks
+sudo apt install gnome-tweak-tool
+cd %% mkdir ~/.themes
+cd && mkdir ./icons
+sudo apt install dconf-editor
+
+# More soon.
