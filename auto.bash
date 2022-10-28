b39652bbc9d5581f4d4445a826eb61332d19460b
theus6 <msantossilva005@gmail.com>
2022-10-28 19:54:39 -0300

    Spotify and Arc Themes update

--- auto.bash
+++ auto.bash
@@ -32,11 +32,26 @@ wget https://notion.davidbailey.codes/notion-linux.repo
 sudo mv notion-linux.repo /etc/zypp/repos.d/notion-linux.repo
 sudo zypper install notion-desktop
 
+# Install Spotify
+wget -O- https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+sudo apt install spotify-client
+
 # Install tweaks
 sudo apt install gnome-tweaks
 sudo apt install gnome-tweak-tool
 cd %% mkdir ~/.themes
 cd && mkdir ./icons
 sudo apt install dconf-editor
+
+# Install Arc Theme
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install arc-theme
+
+# Install Arc Icons
+git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme
+./autogen.sh --prefix=/usr
+sudo make install
 
 # More soon.
